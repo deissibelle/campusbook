93e185a1eed1b8bf25fe4b5e9468d3d097881c85
deissibelle <122529665+deissibelle@users.noreply.github.com>
2023-12-28 10:17:12 +0100

    merge

--- lib/bloc/account/bloc/account_event.dart
+++
@@ -1,4 +0,0 @@
-part of 'account_bloc.dart';
-
-@immutable
-sealed class AccountEvent {}
--- lib/bloc/account/bloc/account_state.dart
+++
@@ -1,6 +0,0 @@
-part of 'account_bloc.dart';
-
-@immutable
-sealed class AccountState {}
-
-final class AccountInitial extends AccountState {}
